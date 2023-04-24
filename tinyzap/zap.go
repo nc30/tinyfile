1bbdd57922d01ae03cbc9e5515ed6a90d29a976a
Asahi Himura <himura@nitolab.com>
2023-04-24 18:20:53 +0900

    add sync

--- tinyzap/zap.go
+++ tinyzap/zap.go
@@ -3,10 +3,19 @@ package tinyzap
 import (
 	"net/url"
 	"path/filepath"
+	"sync"
 
 	"github.com/nc30/tinyfile"
 	"go.uber.org/zap"
 )
+
+var once sync.Once
+
+func init() {
+	once.Do(func() {
+		zap.RegisterSink("tinyfile", TinyFactory)
+	})
+}
 
 func TinyFactory(path *url.URL) (zap.Sink, error) {
 	return tinyfile.NewWriter(filepath.Join(path.Host, path.Path))
